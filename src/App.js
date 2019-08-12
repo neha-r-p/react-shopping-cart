7ec87811ef130742c3880671ee27f5642f06fb15
Neha Patel <neha.r.p@gmail.com>
2019-08-12 14:03:20 -0700

    added value prop to CartContext.Provider

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products] = useState(data);
@@ -19,17 +20,13 @@ function App() {
 
   return (
     <div className="App">
-      <Navigation cart={cart} />
-
-      {/* Routes */}
       <ProductContext.Provider value={{ products, addItem }}>
-        <Route
-          exact
-          path="/"
-          component={Products} />}
-        />
-
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        <CartContext.Provider value={ [ cart, setCart ] }>
+          <Navigation cart={cart} />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />} />
+          <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
